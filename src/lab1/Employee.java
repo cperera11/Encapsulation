416df00b698dae28eaf55c59dfffb57a0e6065d8
cperera <cperera@L11721043664.wctc.msft>
2017-01-31 20:59:10 -0600

    fixed the class for encapsulation

--- src/lab1/Employee.java
+++ src/lab1/Employee.java
@@ -12,15 +12,107 @@ import java.util.Date;
  * @version     1.02
  */
 public class Employee {
-    String firstName;
-    String lastName;
-    public String ssn;
-    boolean metWithHr;
-    boolean metDeptStaff;
-    boolean reviewedDeptPolicies;
-    boolean movedIn;
-    String cubeId;
-    Date orientationDate;
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private boolean metWithHr;
+    private boolean metDeptStaff;
+    private boolean reviewedDeptPolicies;
+    private boolean movedIn;
+    private String cubeId;
+    private Date orientationDate;
+
+   
+ 
+    public Employee(){
+        
+    }
+    
+    public void setFirstName(String firstName) {
+        if (firstName == null || firstName.isEmpty()){
+            System.out.println("Please enter the first name again: ");
+        
+        }else{
+            this.firstName = firstName;
+        }
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName == null || lastName.isEmpty()){
+           System.out.println("Please enter the last name again: "); 
+        }else{
+        this.lastName = lastName;
+        }
+    }
+
+    public void setSsn(String ssn) {
+        if ((ssn == null || ssn.isEmpty()) && (ssn.trim().length() < 9) ){
+           System.out.println("Please enter the SSN again: "); 
+        }else{
+        this.ssn = ssn;
+    }
+    }
+    public void setMetWithHr(boolean metWithHr) {
+        this.metWithHr = metWithHr;
+    }
+
+    public void setMetDeptStaff(boolean metDeptStaff) {
+        this.metDeptStaff = metDeptStaff;
+    }
+
+    public void setReviewedDeptPolicies(boolean reviewedDeptPolicies) {
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+    }
+
+    public void setMovedIn(boolean movedIn) {
+        this.movedIn = movedIn;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    public void setOrientationDate(Date orientationDate) {
+        this.orientationDate = orientationDate;
+    }
+    
+    
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public boolean isMetWithHr() {
+        return metWithHr;
+    }
+
+    public boolean isMetDeptStaff() {
+        return metDeptStaff;
+    }
+
+    public boolean isReviewedDeptPolicies() {
+        return reviewedDeptPolicies;
+    }
+
+    public boolean isMovedIn() {
+        return movedIn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public Date getOrientationDate() {
+        return orientationDate;
+    }
 
 
 
